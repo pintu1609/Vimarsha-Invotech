c1b6a5c171c087531ca4e24a715d794c4100c696
Pintu <pintukumar808284@gmail.com>
2025-12-19 17:34:13 +0530

    site map fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,27 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://vimarsha-invotech.vercel.app", priority: 1 },
-    { url: "https://vimarsha-invotech.vercel.app/about" },
-    { url: "https://vimarsha-invotech.vercel.app/services" },
-    { url: "https://vimarsha-invotech.vercel.app/vision" },
-    { url: "https://vimarsha-invotech.vercel.app/contact" },
+    {
+      url: "https://vimarsha-invotech.vercel.app/",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://vimarsha-invotech.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://vimarsha-invotech.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://vimarsha-invotech.vercel.app/vision",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://vimarsha-invotech.vercel.app/contact",
+      lastModified: new Date(),
+    },
   ];
 }
